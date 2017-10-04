2eae62a19bb9606884069f87d220349b32679b61
Sarah Shuey <sarah.shuey@me.com>
2017-10-04 11:04:27 -0500

    basic page up

---
+++ hackathon-fashion-wardrobe/src/components/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+
+export default class Home extends Component {
+  render(){
+      return(
+        <div className="step1">
+        <h2> Step 1: Upload Clothes</h2>
+        <br></br>
+        </div>
+        <div className="step2">
+        <h2> Step 2: Checkout Todays Look</h2>
+        <br></br>
+        </div>
+        <div className="step3">
+        <h2> Step 3: Get Stylin'</h2>
+        </div>
+      )}}
