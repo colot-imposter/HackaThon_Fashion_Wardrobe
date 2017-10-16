e1791979b2ee80869de3e4784d0cb622ffffb8b4
Colton Dowling <Colton@cd3style.com>
2017-10-16 18:00:01 -0500

    instaled new weather api with logs to define where we might use the data

--- hackathon-fashion-wardrobe/src/components/TodaysLook.js
+++ hackathon-fashion-wardrobe/src/components/TodaysLook.js
@@ -4,114 +4,130 @@ import { connect } from "react-redux";
 import Login from "../containers/Login.js";
 import Register from "../containers/Register.js";
 import UserInfo from "../containers/UserInfo.js";
-import apiKey from './apiKey';
-import img1 from '../styles/imgs/shirts/blue-shortsleeve-light.jpeg'
-import img2 from '../styles/imgs/shirts/white-sleevless-light.jpeg'
-import img3 from '../styles/imgs/shirts/black-shortsleeve-light.jpeg'
+import apiKey from "./apiKey";
+import img1 from "../styles/imgs/shirts/blue-shortsleeve-light.jpeg";
+import img2 from "../styles/imgs/shirts/white-sleevless-light.jpeg";
+import img3 from "../styles/imgs/shirts/black-shortsleeve-light.jpeg";
 
 import { loadTokenFromCookie } from "../actions/actions";
 
 const API_KEY = apiKey;
 console.log(API_KEY);
-const countrycode = 'us';
-const zipcode = '78701';
+const countrycode = "us";
+const zipcode = "78704";
 
-let clothesstyle =  {
-display:"flex",
-flexDirection:"row",
+let clothesstyle = {
+  display: "flex",
+  flexDirection: "row"
 };
 
-let tempstyle =  {
-display:"flex",
-flexDirection:"row-reverse",
-justifyContent:"end",
-color:"#837095",
-fontFamily:"Ubuntu, sans-serif",
-paddingTop:20
+let tempstyle = {
+  display: "flex",
+  flexDirection: "row-reverse",
+  justifyContent: "end",
+  color: "#837095",
+  fontFamily: "Ubuntu, sans-serif",
+  paddingTop: 20
 };
 
-let headstyle =  {
-display:"flex",
-flexDirection:"column",
-color:"#837095",
-fontFamily:"Ubuntu, sans-serif",
-paddingRight:"20px"
+let headstyle = {
+  display: "flex",
+  flexDirection: "column",
+  color: "#837095",
+  fontFamily: "Ubuntu, sans-serif",
+  paddingRight: "20px"
 };
 
-let filterstyle =  {
-display:"flex",
-fontFamily:"Ubuntu, sans-serif",
-flexDirection:"column",
-paddingRight:'155px',
-color:"#837095",
-fontSize:"20px"
+let filterstyle = {
+  display: "flex",
+  fontFamily: "Ubuntu, sans-serif",
+  flexDirection: "column",
+  paddingRight: "155px",
+  color: "#837095",
+  fontSize: "20px"
 };
 
-let pagestyle =  {
-display:"flex",
-flexDirection:"row",
-padding:'20px',
-alignItems:"center",
-alignText:"center",
-justifyContent:"center"
+let pagestyle = {
+  display: "flex",
+  flexDirection: "row",
+  padding: "20px",
+  alignItems: "center",
+  alignText: "center",
+  justifyContent: "center"
 };
 export default class TodaysLook extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
       temperature: "",
-      conditions:""
-    }
+      conditions: ""
+    };
+  }
 
-}
-    componentDidMount() {
+  componentDidMount() {
+    let url = `https://api.apixu.com/v1/forecast.json?key=${API_KEY}&q=${zipcode}`;
 
-      let url = `http://api.openweathermap.org/data/2.5/weather?q= austin,${countrycode}&appid=${API_KEY}`;
+    fetch(url)
+      .then(r => r.json())
+      .then(data => {
+        const maxT = data.forecast.forecastday["0"].day.maxtemp_f;
+        const avgT = data.forecast.forecastday["0"].day.avgtemp_f;
+        const rainInches = data.forecast.forecastday["0"].day.totalprecip_in;
+        console.log("Full data", data);
+        console.log("maxT", maxT);
+        console.log("avgT", avgT);
+        console.log("total precipation expected", rainInches);
 
-        fetch(url)
-          .then(r => r.json())
-          .then((data) => {
-            console.log(data);
-            console.log(data.main.temp)
-            console.log( (1.8 *((data.main.temp)  - 273)) + 32.);
-            this.setState({temperature: ((1.8 *((data.main.temp)  - 273)) + 32.).toFixed(0), conditions: data.weather[0].description})
-      })
-    }
-
+        this.setState({
+          temperature: avgT.toFixed(0),
+          conditions: data.current.condition.text
+        });
+      });
+  }
 
   render() {
-
     return (
       <div className="todaysWeather">
-      <div className="weather" style={tempstyle}>
-        <div className="header" style={headstyle}>
-          <h1>Todays Weather</h1>
-        <div style={{paddingLeft:"20px",paddingRight:"20px", paddingTop:"0px"}}>
-        <h2>{this.state.temperature} Degrees</h2>
-        <h2>{this.state.conditions}</h2>
+        <div className="weather" style={tempstyle}>
+          <div className="header" style={headstyle}>
+            <h1>Todays Weather</h1>
+            <div
+              style={{
+                paddingLeft: "20px",
+                paddingRight: "20px",
+                paddingTop: "0px"
+              }}
+            >
+              <h2>{this.state.temperature} Degrees</h2>
+              <h2>{this.state.conditions}</h2>
+            </div>
+          </div>
+          <div>
+            <img
+              style={{ width: "75px", paddingRight: "20px" }}
+              src="https://thesunshine.co/assets/img/sun.png"
+              alt="Its Sunny!"
+            />
+          </div>
         </div>
-        </div>
-        <div>
-          <img style={{width:"75px", paddingRight:"20px"}}
-            src="https://thesunshine.co/assets/img/sun.png"
-            alt="Its Sunny!"/>
-        </div>
-        </div>
-        <div className= 'previewbody' style={pagestyle}>
+        <div className="previewbody" style={pagestyle}>
           <div className="todaypreview" style={clothesstyle}>
-            <img style={{width:"200px", height:"200px",paddingRight:"40px"}}
-            src={img1}
-            alt="blue-shortsleeve-light"
-          />
-            <img style={{width:"200px", height:"200px",paddingRight:"40px"}}
-            src={img2}
-            alt="white-sleevless-light"
-          />
-            <img style={{width:"200px", height:"200px",paddingRight:"40px"}}
-            src={img3}
-            alt="black-shortsleeve-light"
-          />
+            <img
+              style={{ width: "200px", height: "200px", paddingRight: "40px" }}
+              src={img1}
+              alt="blue-shortsleeve-light"
+            />
+            <img
+              style={{ width: "200px", height: "200px", paddingRight: "40px" }}
+              src={img2}
+              alt="white-sleevless-light"
+            />
+            <img
+              style={{ width: "200px", height: "200px", paddingRight: "40px" }}
+              src={img3}
+              alt="black-shortsleeve-light"
+            />
           </div>
         </div>
       </div>
