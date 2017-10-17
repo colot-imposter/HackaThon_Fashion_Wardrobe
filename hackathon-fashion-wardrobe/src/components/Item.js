cca23c63de3b135d63b7070cfe82a0deeddeefd0
Colton Dowling <Colton@cd3style.com>
2017-10-17 17:12:01 -0500

    connected input Item page

--- hackathon-fashion-wardrobe/src/components/Item.js
+++ hackathon-fashion-wardrobe/src/components/Item.js
@@ -1,28 +1,42 @@
 import React, { Component } from "react";
 
-
 export default class Item extends Component {
-render(){
-return (
-  <div>
-  <form onSubmit={this.handleSubmit}>
-        <label>
-          Sleeve Length:
-          <select value={this.state.value} onChange={this.handleChange}>
-            <option value="Short">Short Sleeve</option>
-            <option value="Long">Long Sleeve</option>
-            <option value="No">No Sleeve</option>
-          </select>
-        </label>
-        <label>
-          Weight:
-          <select value={this.state.value} onChange={this.handleChange}>
-            <option value="Light">Light Weight</option>
-            <option value="Medium">Medium Weight</option>
-            <option value="Heavy">Heavy Weight</option>
-          </select>
-        </label>
-        <label>
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sleeveLength: "",
+      fabricWeight: ""
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Sleeve Length:
+            <select
+              value={this.state.sleeveLength}
+              onChange={this.handleChange}
+            >
+              <option value="Short">Short Sleeve</option>
+              <option value="Long">Long Sleeve</option>
+              <option value="No">No Sleeve</option>
+            </select>
+          </label>
+          <label>
+            Weight:
+            <select
+              value={this.state.fabricWeight}
+              onChange={this.handleChange}
+            >
+              <option value="Light">Light Weight</option>
+              <option value="Medium">Medium Weight</option>
+              <option value="Heavy">Heavy Weight</option>
+            </select>
+          </label>
+          {/* <label>
           Color:
           <select value={this.state.value} onChange={this.handleChange}>
             <option value="Black">Black</option>
@@ -31,14 +45,14 @@ return (
             <option value="Blue">Blue</option>
             <option value="Pattern">Pattern</option>
           </select>
-        </label>
-        <input type="submit" value="Add Item" />
-      </form>
-      <div className="description">
-      <p> * insert image of short long and no sleeve top </p>
-      <p> * insert image of light mid and heavy weight top </p>
+        </label> */}
+          <input type="submit" value="Add Item" />
+        </form>
+        <div className="description">
+          <p> * insert image of short long and no sleeve top </p>
+          <p> * insert image of light mid and heavy weight top </p>
+        </div>
       </div>
-      </div>
-  )
-}
+    );
+  }
 }
