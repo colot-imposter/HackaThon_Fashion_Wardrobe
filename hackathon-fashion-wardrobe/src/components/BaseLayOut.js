5a3be08f31053ab3f302df8d5bfbc364fa4e1fdd
Sarah Shuey <sarah.shuey@me.com>
2017-10-19 10:44:11 -0500

    login styling done

--- hackathon-fashion-wardrobe/src/components/BaseLayOut.js
+++ hackathon-fashion-wardrobe/src/components/BaseLayOut.js
@@ -36,9 +36,9 @@ export default class BaseLayout extends Component {
               <nav className="row navbar navbar-inverse">
                 <div className="container-fluid">
                 <div style={border} className="navbar-header">
-                  <a href="/"><img src={logo} alt="logo" style={{paddingTop:"15px",paddingLeft:"30px",float:"left"}}></img></a>
+                  <a href="/User_Dashboard"><img src={logo} alt="logo" style={{paddingTop:"15px",paddingLeft:"30px",float:"left",height:"80px"}}></img></a>
                     <div className="dropdown">
-                      <button className="dropbtn"><img src={hamPic} alt="Menu"></img></button>
+                      <button className="dropbtn"><img src={hamPic} style={{height:"60px"}} alt="Menu"></img></button>
                       <div className="dropdown-content">
                         <a href="/">Home</a>
                         <a href="/Login">Login</a>
