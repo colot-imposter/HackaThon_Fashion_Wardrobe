79ebcab6c3d3115a2d853079d31731dc63e5f98d
Sarah Shuey <sarah.shuey@me.com>
2017-10-24 17:16:29 -0500

    added hourly forcast

--- hackathon-fashion-wardrobe/src/components/weather.js
+++ hackathon-fashion-wardrobe/src/components/weather.js
@@ -31,6 +31,13 @@ let headstyle = {
   paddingRight: "20px",
   justifyContent:"center"
 };
+let timestyle={
+  display:"flex",
+  flexDirection:"row",
+  justifyContent:"center",
+  paddingRight:"10px",
+  paddingBottom:"10px"
+}
 
 export default class Weather extends Component {
   constructor(props) {
@@ -55,13 +62,27 @@ export default class Weather extends Component {
         const rainInches = data.forecast.forecastday["0"].day.totalprecip_in;
         const city = data.location.name;
         const state = data.location.region;
+        const sun = data.forecast.forecastday["0"].astro.sunset;
+        const hour6= data.forecast.forecastday["0"].hour[6].temp_f;
+        const hour7= data.forecast.forecastday["0"].hour[7].temp_f;
+        const hour8= data.forecast.forecastday["0"].hour[8].temp_f;
+        const hour9= data.forecast.forecastday["0"].hour[9].temp_f;
+        const hour10= data.forecast.forecastday["0"].hour[10].temp_f;
+        const hour11= data.forecast.forecastday["0"].hour[11].temp_f;
+        const hour12= data.forecast.forecastday["0"].hour[12].temp_f;
+        const hour13= data.forecast.forecastday["0"].hour[13].temp_f;
+        const hour14= data.forecast.forecastday["0"].hour[14].temp_f;
+        const hour15= data.forecast.forecastday["0"].hour[15].temp_f;
+        const hour16= data.forecast.forecastday["0"].hour[16].temp_f;
+        const hour17= data.forecast.forecastday["0"].hour[17].temp_f;
+        const hour18= data.forecast.forecastday["0"].hour[18].temp_f;
+        const hour19= data.forecast.forecastday["0"].hour[19].temp_f;
 
-        console.log(data);
-        console.log("feelsLikeT", feelsLikeT);
+
+
+
+
         console.log("Full data", data);
-        console.log("maxT", maxT);
-        console.log("avgT", avgT);
-        console.log("total precipation expected", rainInches);
 
         this.setState({
           temperature: avgT.toFixed(0),
@@ -70,19 +91,56 @@ export default class Weather extends Component {
           highTemp: maxT.toFixed(0),
           lowTemp: minT.toFixed(0),
           city: city,
-          state: state
+          state: state,
+          sunset: sun,
+          hour6:hour6.toFixed(0),
+          hour7:hour7.toFixed(0),
+          hour8:hour8.toFixed(0),
+          hour9:hour9.toFixed(0),
+          hour10:hour10.toFixed(0),
+          hour11:hour11.toFixed(0),
+          hour12:hour12.toFixed(0),
+          hour13:hour13.toFixed(0),
+          hour14:hour14.toFixed(0),
+          hour15:hour15.toFixed(0),
+          hour16:hour16.toFixed(0),
+          hour17:hour17.toFixed(0),
+          hour18:hour18.toFixed(0),
+          hour19:hour19.toFixed(0),
         });
       });
   }
   render(){
     return(
       <div className="weather" style={headstyle}>
-      <h2 style={tempstyle}> Weather In {this.state.city} {this.state.state}</h2>
-      <h2 style={tempstyle}> Currnet Temperature: {this.state.temperature} Degrees</h2>
-      <h3 style={tempstyle}>Feels Like:  {this.state.feelsLikeTemp} Degrees</h3>
+      <h2 style={tempstyle}> Weather In {this.state.city},{this.state.state}</h2>
+      <h2 style={tempstyle}> Currnet Temperature: {this.state.temperature}°</h2>
+      <h3 style={tempstyle}>Feels Like:  {this.state.feelsLikeTemp}°</h3>
       <h3 style={tempstyle}> Conditions Outside: {this.state.conditions} </h3>
-      <h3 style={tempstyle}> Todays High: {this.state.highTemp} Degrees</h3>
-      <h3 style={tempstyle}> Todays Low: {this.state.lowTemp} Degrees</h3>
+      <h3 style={tempstyle}> Todays High: {this.state.highTemp}°</h3>
+      <h3 style={tempstyle}> Todays Low: {this.state.lowTemp}°</h3>
+      <h3 style={tempstyle}> The Sun Will Set at: {this.state.sunset}</h3>
+      <h3 style={tempstyle}> Hourly forcast </h3>
+      <div style={timestyle}>
+      <h4 style={timestyle}> 6 am<br></br>{this.state.hour6}°</h4>
+      <h4 style={timestyle}> 7 am<br></br>{this.state.hour7}°</h4>
+      <h4 style={timestyle}> 8 am<br></br>{this.state.hour8}°</h4>
+      <h4 style={timestyle}> 9 am<br></br>{this.state.hour9}°</h4>
+      <h4 style={timestyle}> 10 am<br></br>{this.state.hour10}°</h4>
+      <h4 style={timestyle}> 11 am<br></br>{this.state.hour11}°</h4>
+      <h4 style={timestyle}> 12 pm<br></br>{this.state.hour12}°</h4>
+      <h4 style={timestyle}> 1 pm<br></br>{this.state.hour13}°</h4>
+      <h4 style={timestyle}> 2 pm<br></br>{this.state.hour14}°</h4>
+      <h4 style={timestyle}> 3 pm<br></br>{this.state.hour15}°</h4>
+      <h4 style={timestyle}> 4 pm<br></br>{this.state.hour16}°</h4>
+      <h4 style={timestyle}> 5 pm<br></br>{this.state.hour17}°</h4>
+      <h4 style={timestyle}> 6 pm<br></br>{this.state.hour18}°</h4>
+      <h4 style={timestyle}> 7 pm<br></br>{this.state.hour19}°</h4>
+
+
+
+
+      </div>
       </div>
     )
   }
