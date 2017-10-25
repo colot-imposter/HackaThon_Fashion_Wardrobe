341a0d7e6d00d2470e2ace31c7c5d71676426921
Sarah Shuey <sarah.shuey@me.com>
2017-10-25 12:59:08 -0500

    fixed drop down styles

--- hackathon-fashion-wardrobe/src/components/BaseLayOut.js
+++ hackathon-fashion-wardrobe/src/components/BaseLayOut.js
@@ -10,7 +10,8 @@ export default class BaseLayout extends Component {
   "borderBottomColor":"#837095",
   "borderBottomStyle":"solid",
   "postion":"relative",
-  "overflow":"hidden"
+  "overflow":"hidden",
+  "outline":"none"
 }
 
     return (
@@ -20,9 +21,9 @@ export default class BaseLayout extends Component {
               <nav className="row navbar navbar-inverse">
                 <div className="container-fluid">
                 <div style={border} className="navbar-header">
-                  <a href="/User_Dashboard"><img src={logo} alt="logo" style={{paddingTop:"15px",paddingLeft:"30px",float:"left",height:"80px"}}></img></a>
+                  <a href="/User_Dashboard"><img src={logo} alt="logo" className="logoimg"></img></a>
                     <div className="dropdown">
-                      <button className="dropbtn"><img src={hamPic} style={{height:"60px"}} alt="Menu"></img></button>
+                      <button className="dropbtn"><img src={hamPic} alt="Menu"></img></button>
                       <div className="dropdown-content">
                         <a href="/User_Dashboard">Dashboard</a>
                         <a href="/TodaysLook">Todays Look</a>
