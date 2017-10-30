adccc1ae07e61ece19724a65769d7a2e250b7cf2
Colton Dowling <Colton@cd3style.com>
2017-10-30 17:08:20 -0500

    changed exported function of fetchClothingitem to fetchClothingItem

--- hackathon-fashion-wardrobe/src/components/updateItem.js
+++ hackathon-fashion-wardrobe/src/components/updateItem.js
@@ -1,24 +1,60 @@
 import React, { Component } from "react";
 import Form from "./form";
+import { fetchClothingItem, updateClothingItem } from "../actions/crud";
 
 export default class updateItem extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      sleeveLength: "",
-      fabricWeight: ""
+      clothingItem: {}
     };
   }
+
+  componentDidMount() {
+    fetchClothingItem(this.props.params.postId)
+      .then(data => {
+        this.setState(state => {
+          state.clothingItem = data;
+          return state;
+        });
+      })
+      .catch(err => {
+        console.error("err", err);
+      });
+  }
+
+  handleSubmit(data) {
+    updateClothingItem(this.state.blogPost.id, data);
+    this.props.router.push("/");
+  }
+
   render() {
     return (
-      <div className="updateForm">
+      <div>
         <h1>Update Item</h1>
-
-        <Form />
-
-        <button>Delete Item</button>
+        <Form
+          onSubmit={this.handleSubmit.bind(this)}
+          title={this.state.clothingItem.name}
+          body={this.state.clothingItem.fabricWeight}
+        />
       </div>
     );
   }
 }
+
+// {this.props.clothingItem && this.props.clothingItem.map((post, i) => {
+//                 return (
+//                     <tr key={post.id}>
+//                         <td>{post.id}</td>
+//                         <td>{post.title}</td>
+//                         <td>
+//                             <Link to={`/posts/update/${post.id}`} className="btn btn-default btn-sm">Edit
+//                           </Link>
+//
+//                             <btn onClick={this.deleteHandler.bind(this, i)}>
+//                             Delete
+//                           </btn>
+//
+//                         );
+// })}
