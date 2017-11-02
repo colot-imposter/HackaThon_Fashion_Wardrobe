6888a7d41c8b263805145bf1222ef7541507b865
Colton Dowling <Colton@cd3style.com>
2017-11-02 10:10:52 -0500

    updated baselayout css class names

--- hackathon-fashion-wardrobe/src/components/BaseLayOut.js
+++ hackathon-fashion-wardrobe/src/components/BaseLayOut.js
@@ -16,40 +16,36 @@ export default class BaseLayout extends Component {
     return (
       <div className="BaseLayout">
         <div>
-          <div className="body">
-            <nav className="row navbar navbar-inverse">
-              <div className="container-fluid">
-                <div style={border} className="navbar-header">
-                  <a href="/TodaysLook">
-                    <img src={logo} alt="logo" className="logoimg" />
-                  </a>
-                  <div className="dropdown">
-                    <button className="dropbtn">
-                      <img src={hamPic} alt="Menu" />
-                    </button>
-                    <div className="dropdown-content">
-                      <a href="/TodaysLook">Todays Look</a>
-                      <a href="/Wardrobe">Wardrobe</a>
-                      <a href="/newItem">Add Item</a>
-                      <a href="/help">Help</a>
-
-                      <a href="/Login">Login</a>
-                      <a href="/Logout">Logout</a>
-                    </div>
+          <nav className="row navbar navbar-inverse">
+            <div className="container-fluid">
+              <div style={border} className="navbar-header">
+                <a href="/TodaysLook">
+                  <img src={logo} alt="logo" className="logoimg" />
+                </a>
+                <div className="dropdown">
+                  <button className="dropbtn">
+                    <img src={hamPic} alt="Menu" />
+                  </button>
+                  <div className="dropdown-content">
+                    <a href="/TodaysLook">Todays Look</a>
+                    <a href="/Wardrobe">Wardrobe</a>
+                    <a href="/newItem">Add Item</a>
+                    <a href="/Login">Login</a>
+                    <a href="/Logout">Logout</a>
                   </div>
                 </div>
               </div>
-            </nav>
-
-            {this.props.children}
-            <div>
-              <footer>
-                <p className="name">Tunic inc.</p>
-                <p className="help">
-                  <a href="/help">Help</a>
-                </p>
-              </footer>
             </div>
+          </nav>
+
+          {this.props.children}
+          <div>
+            <footer>
+              <p className="name">Tunic inc.</p>
+              <p className="help">
+                <a href="/help">Help</a>
+              </p>
+            </footer>
           </div>
         </div>
       </div>
